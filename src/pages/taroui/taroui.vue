83285a401e3e83dd5e8523ebd2cfc20fdecd3df6
zhuxiuwu <zhuxw@133.cn>
2021-02-24 14:59:50 +0800

    函数传递参数

--- src/pages/taroui/taroui.vue
+++ src/pages/taroui/taroui.vue
@@ -2,29 +2,58 @@
 <template>
   <view>
     <AtButton type='primary' :onClick="handleClick">按钮文案</AtButton>
+    <AtActionSheet
+      cancelText='取消'
+      :isOpened="isOpened"
+      :on-cancel="handleCancel"
+      :on-close="handleClose"
+      title='清除位置信息后， 别人将不能查看到你'
+    >
+      <AtActionSheetItem
+        :on-click="()=>clickBtn(1)"
+      >
+        按钮一
+      </AtActionSheetItem>
+      <AtActionSheetItem
+        :on-click="()=>clickBtn(2)"
+      >
+        按钮二
+      </AtActionSheetItem>
+      <AtActionSheetItem
+        :on-click="()=>clickBtn(3)"
+      >
+        <text class='danger'>清除位置信息并退出</text>
+      </AtActionSheetItem>
+    </AtActionSheet>
   </view>
 </template>
 
 <script>
-import { AtButton } from 'taro-ui-vue'
+import { AtButton,AtActionSheet,AtActionSheetItem } from 'taro-ui-vue'
 export default {
-
-
   name: "taroui",
-
   components: {
-      AtButton
+      AtButton,AtActionSheet,AtActionSheetItem
   },
-
   data() {
-    return {};
+    return {
+      isOpened:false
+    };
   },
-
   computed: {},
-
   methods: {
     handleClick () {
-      console.log('click button')
+      this.isOpened = true;
+    },
+    handleCancel(){
+      this.isOpened = false;
+    },
+    handleClose(){
+      this.isOpened = false;
+    },
+    clickBtn(data){
+      debugger
+      console.log(data);
     }
   },
   onShow() {
